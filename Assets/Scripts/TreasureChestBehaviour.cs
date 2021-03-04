bfa3a508dcbddf779426183ff3efb452928b56e9
Kjumar <jganguli@hotmail.ca>
2021-03-04 01:35:55 -0500

    added stars. Treasure chest spawns a lot of them

--- Assets/Scripts/TreasureChestBehaviour.cs
+++ Assets/Scripts/TreasureChestBehaviour.cs
@@ -6,17 +6,29 @@ public class TreasureChestBehaviour : MonoBehaviour
 {
     public bool isOpen = false;
     [SerializeField] private Sprite openChest;
+    [SerializeField] private GameObject loot;
 
     private SpriteRenderer sr;
+    private float spawnTimer = 0f;
 
     private void Start()
     {
         sr = GetComponent<SpriteRenderer>();
     }
 
-    void Update()
+    void FixedUpdate()
     {
-        
+        if (isOpen)
+        {
+            spawnTimer += Time.fixedDeltaTime;
+            if (spawnTimer >= 0.2f)
+            {
+                spawnTimer = 0f;
+                GameObject newLoot = Instantiate(loot, transform);
+                newLoot.transform.position = newLoot.transform.position + new Vector3(0, 0, -1);
+                newLoot.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, 800));
+            }
+        }
     }
 
     public void Open()
